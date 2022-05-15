95624977322f236f36e5d1d76c9a20e69fb2a418
jbellon <josh@joshbellon.com>
2022-05-15 13:54:12 -0700

    Day 1 homework for django collector project is complete.  HOWEVER the django framework can't seem to locate my views folde at the moment.

--- comicbookcollector/main_app/views.py
+++ comicbookcollector/main_app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Comic:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, series, title, issue_num, publisher, year):
+    self.series = series
+    self.title = title
+    self.issue_num = issue_num
+    self.publisher = publisher
+    self.year = year
+
+comic = [
+  Comic('Superman', 'The Day Superman Ate Gluten', '100', 'DC', '1995'),
+  Comic('Transformers', 'Return of the Decepticons', '24', 'Marvel', '1989'),
+  Comic('Teenage Mutant Ninja Turtles', 'The Secret of the Ooze', '5', 'Archie', '1991')
+]
+
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About the Comic Book Collector</h1>')
+    return render(request, 'about.html')
+
+def comics_index(request):
+    return render(request, 'cats/index.html', { 'comics': comic })
